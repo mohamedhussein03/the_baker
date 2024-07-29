33cbaeb9a4cc11456fd7461e835f4a6a2986ef93
mohamedhussein03 <mohamed219813@bue.edu.eg>
2024-07-29 21:57:11 +0300

    new changes

---
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -0,0 +1,17 @@
+import 'dart:developer';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:user_repository/user_repository.dart';
+
+
+class FirebaseUserRepo implements UserRepository {
+  final FirebaseAuth _firebaseAuth;
+	final usersCollection = FirebaseFirestore.instance.collection('users');
+
+  FirebaseUserRepo({
+    FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+  
+  
+  }
